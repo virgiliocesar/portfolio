dce7d26e5d47d8f2e950d5fdd1f130981e012b66
Virgílio César <enjels@outlook.com>
2024-02-20 10:01:06 -0300

    teste

--- src/components/container_Global/Container.jsx
+++ src/components/container_Global/Container.jsx
@@ -2,7 +2,7 @@ import "./Container.css"
 import Header from "../header/Header";
 import Sobremim from "../Sobremim/Sobremim";
 import Projetos from "../projetos/Projetos";
-import Hardskills from "../hard_skills/hardskills";
+// import Hardskills from "../hard_skills/hardskills";
 import Softskills from "../Soft_skills/Softskills";
 
 const Container = () => {
@@ -10,7 +10,7 @@ const Container = () => {
       <div id="main-container">
         <Header />
         <Sobremim />
-        <Hardskills />
+        {/* <Hardskills /> */}
         <Softskills />
         <Projetos />
       </div>
