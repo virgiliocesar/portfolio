e08d9f2d4f9e91bccc8e8c8c54f26720cf9901f7
Virgílio César <enjels@outlook.com>
2024-03-14 05:48:55 -0300

    projeto novo countdown adicionado ao portfolio

--- src/components/projetos/Projetos.jsx
+++ src/components/projetos/Projetos.jsx
@@ -4,13 +4,21 @@ const Projetos = () => {
     {
       id: 1,
       imagem:
+        "https://i.pinimg.com/736x/b4/08/6f/b4086f75d9120567be99b0e1c1e7da26.jpg",
+      link: "https://countdown-bice-six.vercel.app/",
+      codigo: "https://github.com/enjels/countdown",
+      text: "countdown feito em react, contdor regrecivo para datas.",
+    },
+    {
+      id: 2,
+      imagem:
         "https://i.pinimg.com/736x/a0/c5/08/a0c508b90be731186214c878b9267eeb.jpg",
       link: "https://quiz-enjels.vercel.app/",
       codigo: "https://github.com/enjels/quiz",
       text: "Quiz feito em react",
     },
     {
-      id: 2,
+      id: 3,
       imagem:
         "https://i.pinimg.com/736x/87/97/29/87972930fbe39f177ca7ee78c5d9ae9f.jpg",
       link: "https://multstep-form-react.vercel.app/",
@@ -18,7 +26,7 @@ const Projetos = () => {
       text: "multstep-form-react, é um formulario de avaliação, feito em React com json server",
     },
     {
-      id: 3,
+      id: 4,
       imagem:
         "https://i.pinimg.com/736x/f4/3c/ba/f43cba6b9d53721ae24bb6bcea82495c.jpg",
       link: "https://imc-calc-murex.vercel.app/",
@@ -26,7 +34,7 @@ const Projetos = () => {
       text: "Calculadora de IMC, criada em react com json server",
     },
     {
-      id: 4,
+      id: 5,
       imagem:
         "https://i.pinimg.com/736x/72/0b/d6/720bd6b3460dd69e2650f1ccaaffb91f.jpg",
       link: "https://mentorclass.vercel.app/",
@@ -35,7 +43,7 @@ const Projetos = () => {
       text: "Mentor Class, uma area de gestão de alunos e professores e turmas para ensino, possui registro de email alunos e professores, Desenvovido com HTML, CSS e javaScript.",
     },
     {
-      id: 5,
+      id: 6,
       imagem:
         "https://i.pinimg.com/736x/ed/3a/3a/ed3a3a925bd3c48b84736a9c14a2608c.jpg",
       link: "https://enjels.github.io/codai-2.0/",
@@ -43,7 +51,7 @@ const Projetos = () => {
       text: "Projeto Codai -2.0 Nikel, é uma carteira digital,que registra a entra e saída do saldo da conta, detalhes descrição da transação e data do processo tudo isso sendo salvo no Histórico, possui cadastro e login, usa local storage para salvar os dados, Desenvovido com HTML, CSS e javaScript..",
     },
     {
-      id: 6,
+      id: 7,
       imagem:
         "https://i.pinimg.com/736x/db/c1/31/dbc1318f799988e3d8839231b2e5e92f.jpg",
       link: "https://enjels.github.io/Coffee-Shop/",
@@ -52,7 +60,7 @@ const Projetos = () => {
     },
 
     {
-      id: 7,
+      id: 8,
       imagem:
         "https://i.pinimg.com/736x/28/0b/64/280b643cfc46a8501497cb4d6c4b733e.jpg",
       link: "https://enjels.github.io/Doritos/",
@@ -60,7 +68,7 @@ const Projetos = () => {
       text: "Site de doritos um dos primeiros projetos criado quando eu não tinha conhecimento em programação para usar ele como medida de evolução, Desenvovido com HTML, CSS e javaScript. OBS: embreve uma nova versão melhorada.",
     },
     {
-      id: 8,
+      id: 9,
       imagem:
         "https://i.pinimg.com/736x/95/4c/ee/954cee887f7a77d58e0c5bb62e5fa7d4.jpg",
       link: "https://enjels.github.io/relogio-digital/",
@@ -68,7 +76,7 @@ const Projetos = () => {
       text: "Relógio digital criado apartir de uma referencia no youtube, Desenvovido com HTML, CSS e javaScript..",
     },
     {
-      id: 9,
+      id: 10,
       imagem:
         "https://i.pinimg.com/736x/1a/b4/ef/1ab4ef8a882dd49b5bb73788a196aaaf.jpg",
       link: "https://enjels.github.io/lading-page-treino-exclusivo",
@@ -76,7 +84,7 @@ const Projetos = () => {
       text: "Landing page, projeto criado apartir do curso da rocketseat, objetivo aplicar e consolidar conhecimento basicas estudads do html e css.",
     },
     {
-      id: 10,
+      id: 11,
       imagem:
         "https://i.pinimg.com/736x/98/14/47/98144718b992bf509f3864aace65fc30.jpg",
       link: "https://enjels.github.io/formulario-Informacoes",
@@ -84,7 +92,7 @@ const Projetos = () => {
       text: "Formulário projeto criado apartir do curso da rocketseat, objetivo aplicar e consolidar conhecimento em criação de formularios, Desenvovido com HTML, CSS e javaScript..",
     },
     {
-      id: 11,
+      id: 12,
       imagem:
         "https://i.pinimg.com/736x/52/46/6a/52466a0804d28db6eafba09fdc3db356.jpg",
       link: "https://enjels.github.io/login-cat/",
@@ -93,7 +101,7 @@ const Projetos = () => {
     },
 
     {
-      id: 12,
+      id: 13,
       imagem:
         "https://i.pinimg.com/736x/5c/87/73/5c877305897cc5f45b1edfe6b866d961.jpg",
       link: "https://enjels.github.io/responsivo-desktop-Mobile/",
@@ -101,7 +109,7 @@ const Projetos = () => {
       text: "Responsivo desktop Mobile, aprendizado sobre responsividade.",
     },
     {
-      id: 13,
+      id: 14,
       imagem:
         "https://i.pinimg.com/736x/c3/cd/3c/c3cd3c6dcda70b8671fb43bce8484916.jpg",
       link: "https://enjels.github.io/z-index-zoom/",
@@ -109,7 +117,7 @@ const Projetos = () => {
       text: "z-index-zoom, parendizado sobre  z-index .",
     },
     {
-      id: 14,
+      id: 15,
       imagem:
         "https://i.pinimg.com/736x/40/dc/08/40dc08ab9b1978363efd27aded942052.jpg",
       link: "https://enjels.github.io/ambientes-unico/",
@@ -117,7 +125,7 @@ const Projetos = () => {
       text: "ambiente unico,um dos primeiros projetos de estudo para fixar conhecimentos básicos.",
     },
     {
-      id: 15,
+      id: 16,
       imagem:
         "https://www.ccj.ufpb.br/dcx/contents/imagens/figuras/output_zvmmKf.gif/@@images/image.gif",
       link: "#",
